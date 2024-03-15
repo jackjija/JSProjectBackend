5bf894f87b571a38706cb76690e1f1d2868e3603
jackjija <jacker1502@hotmail.com>
2024-03-16 00:18:50 +0700

    a

--- Database.js
+++ Database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const app = express();
 require('dotenv').config();
 
-const port  = process.env.PORT || 5000;
+const port  = process.env.PORT || 3000;
 app.use(express.json());
 
 const sequelize = new Sequelize('database', 'username', 'password', {
